47af463b47d0eabdd0ad46adce6e24bfe5bc4ef5
Dong Zhou <zhou.dong@gmail.com>
2018-01-31 12:55:08 -0500

    add main logic

---
+++ README.md
@@ -0,0 +1,16 @@
+# wrapper around git
+
+Manage multiple git repos
+
+The environment variable `REPOPATH` is used to save paths of the repos.
+
+```
+python3 -m gita
+```
+
+## todo
+
+* gitm ls: display the status of all repos
+    * color
+* gitm add path or environment variable
+* delegate git on each repo
---
+++ gitm/__main__.py
@@ -0,0 +1,71 @@
+import os
+import argparse
+
+from . import ls
+
+
+def is_git(path):
+    return os.path.isdir(os.path.join(path, '.git'))
+
+def update_repos(new_paths=None):
+    """
+    :type new_paths: `list` of `str` or `None`
+
+    :rtype: `dict` of repo name to repo absolute path
+    """
+    try:
+        paths = set(os.environ['REPOPATH'].split(os.pathsep))
+    except KeyError:
+        paths = set()
+        os.environ['REPOPATH'] = ''
+    if new_paths:
+        new_paths = [os.path.abspath(p) for p in new_paths if is_git(p)]
+        print(new_paths)
+        new_paths = set(filter(lambda p: p not in paths, new_paths))
+        print(new_paths)
+        for p in new_paths:
+            os.environ['REPOPATH'] += (os.pathsep + p)
+        print(os.environ['REPOPATH'])
+        paths.update(new_paths)
+    return {os.path.basename(os.path.normpath(p)):p for p in paths}
+
+def f_add(args):
+    repos = update_repos(args.repo)
+    print('add', repos)
+
+
+def f_ls(args):
+    repos = update_repos()
+    print('ls', args, repos)
+
+def f_repo(args):
+    print('repo', args)
+
+
+def main(argv=None):
+    p = argparse.ArgumentParser()
+    subparsers = p.add_subparsers()
+
+    p_ls = subparsers.add_parser('ls', description='here')
+    p_ls.add_argument('repo', nargs='*',
+            help="repo(s) to show")
+    p_ls.set_defaults(func=f_ls)
+
+    p_add = subparsers.add_parser('add')
+    p_add.add_argument('repo', nargs='+', # type=update_repos,
+            help="add repositories")
+    p_add.set_defaults(func=f_add)
+
+    p.add_argument('goto', metavar='repo', nargs='?',
+            # choices=update_repos(),
+            default=None,
+            help="go to the directory of the chosen repo")
+
+    args = p.parse_args(argv)
+
+    if args.goto:
+        print('repos:', args.goto)
+    else:
+        args.func(args)
+
+main()
