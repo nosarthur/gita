54f31224b71b64e4c2c44e2c9400da3fdfc4160d
Dong Zhou <zhou.dong@gmail.com>
2019-02-09 22:19:17 -0500

    use XDG_CONFIG_HOME

--- .gita-completion.bash
+++ .gita-completion.bash
@@ -2,12 +2,16 @@
 _gita_completions()
 {
 
-  local cur=${COMP_WORDS[COMP_CWORD]}
+  local cur commands repos XDG_CONFIG_HOME gita_path
 
-  # this is slow
+  cur=${COMP_WORDS[COMP_CWORD]}
+  XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+  gita_path=${XDG_CONFIG_HOME}/gita/repo_path
+
+  # this is somewhat slow
   commands=`gita -h | sed '2q;d' |sed 's/[{}.,]/ /g'`
   # this doesn't work for two repos with the same basename
-  repos=`awk '{split($0, paths, ":")} END {for (i in paths) {n=split(paths[i],b, /\//); print b[n]}}' ~/.config/gita/repo_path`
+  repos=`awk '{split($0, paths, ":")} END {for (i in paths) {n=split(paths[i],b, /\//); print b[n]}}' ${gita_path}`
 
   if [ $COMP_CWORD -eq 1 ]; then
     COMPREPLY=($(compgen -W "${commands}" ${cur}))
