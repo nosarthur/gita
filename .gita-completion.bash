9e2f8d513b6b4f8a74fdc127eebdafb919d6effc
Dong Zhou <zhou.dong@gmail.com>
2021-06-26 02:10:43 -0400

    add more bash autocompletion

--- .gita-completion.bash
+++ .gita-completion.bash
@@ -7,19 +7,24 @@ _gita_completions()
   cur=${COMP_WORDS[COMP_CWORD]}
   cmd=${COMP_WORDS[1]}
 
-  # FIXME: this is somewhat slow
-  commands=`gita -h | sed '2q;d' |sed 's/[{}.,]/ /g'`
-
   # this doesn't work for two repos with the same basename
   #gita_path=${XDG_CONFIG_HOME:-$HOME/.config}/gita/repo_path
   #repos=`awk '{split($0, paths, ":")} END {for (i in paths) {n=split(paths[i],b, /\//); print b[n]}}' ${gita_path}`
 
   if [ $COMP_CWORD -eq 1 ]; then
+    # FIXME: this is somewhat slow
+    commands=`gita -h | sed '2q;d' |sed 's/[{}.,]/ /g'`
     COMPREPLY=($(compgen -W "${commands}" ${cur}))
   elif [ $COMP_CWORD -gt 1 ]; then
     case $cmd in
       add)
         COMPREPLY=($(compgen -d ${cur}))
+        ;;
+      clone)
+        COMPREPLY=($(compgen -f ${cur}))
+        ;;
+      color | flags)
+        COMPREPLY=($(compgen -W "ll set" ${cur}))
         ;;
       ll | context)
         groups=`gita group ls`
@@ -32,7 +37,6 @@ _gita_completions()
         ;;
     esac
   fi
-
 }
 
 complete -F _gita_completions gita
