edbd2c8606c98120b6b89932f34a007a84b36c9b
Gregory Bloquel <gregory.bloquel@fr.clara.net>
2022-09-13 22:38:42 +0200

    feat(common): env variable for config at project level

--- gita/common.py
+++ gita/common.py
@@ -2,7 +2,7 @@ import os
 
 
 def get_config_dir() -> str:
-    root = os.environ.get('XDG_CONFIG_HOME') or os.path.join(
+    root = os.environ.get('GITA_PROJECT_HOME') or os.environ.get('XDG_CONFIG_HOME') or os.path.join(
         os.path.expanduser('~'), '.config')
     return os.path.join(root, "gita")
 
