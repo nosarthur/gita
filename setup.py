619926e68c769133bdc5b5bae7c0aa65782a4c50
Dong Zhou <zhou.dong@gmail.com>
2023-04-29 22:22:17 -0400

    bump vertion to 16.2.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as f:
 setup(
     name="gita",
     packages=["gita"],
-    version="0.16.2.3",
+    version="0.16.3",
     license="MIT",
     description="Manage multiple git repos with sanity",
     long_description=long_description,
