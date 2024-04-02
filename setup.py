00b050ad1e2180187051ada98a7dbd7c493781db
Dong Zhou <zhou.dong@gmail.com>
2024-04-01 21:36:26 -0400

    enhance freeze and clone for groups

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", encoding="utf-8") as f:
 setup(
     name="gita",
     packages=["gita"],
-    version="0.16.6.5",
+    version="0.16.7",
     license="MIT",
     description="Manage multiple git repos with sanity",
     long_description=long_description,
