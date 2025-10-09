579c4ad60169f6dfb18b1eb0b26ee4fa87f77bc2
ggobbe <ggobbe@users.noreply.github.com>
2025-10-09 08:57:12 +0200

    Update pipelines to test with python 3.7 -> 3.14

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author="Dong Zhou",
     author_email="zhou.dong@gmail.com",
     entry_points={"console_scripts": ["gita = gita.__main__:main"]},
-    python_requires="~=3.7",
+    python_requires=">=3.7",
     install_requires=["argcomplete"],
     classifiers=[
         "Development Status :: 4 - Beta",
@@ -30,11 +30,15 @@ setup(
         "Topic :: Software Development :: Version Control :: Git",
         "Topic :: Terminals",
         "Topic :: Utilities",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
     include_package_data=True,
 )
+
